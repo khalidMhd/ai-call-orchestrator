01ec8128c3b139f351c3c6901f4375acf4be4ac2
khalidmhd <webdev1137@gmail.com>
2025-07-24 20:44:46 +0300

    redis Retry Strategy implemented

--- src/queues/call.queue.ts
+++ src/queues/call.queue.ts
@@ -6,7 +6,13 @@ export const connection = new IORedis({
   maxRetriesPerRequest: null,
   host: REDIS_HOST || 'localhost',
   port: REDIS_PORT || 6379,
+  retryStrategy(times) {
+    const delay = Math.min(times * 50, 2000);
+    return delay; // reconnect delay in ms
+  },
 })
 
+connection.on('connect', () => console.log('Redis connected'));
+connection.on('error', (err) => console.error('Redis error', err.message));
 
 export const callQueue = new Queue('calls', { connection })
