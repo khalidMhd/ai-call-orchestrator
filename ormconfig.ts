6ab04c452a1f097ffa2e8f5027e3b383215cce38
khalidmhd <webdev1137@gmail.com>
2025-07-24 16:18:40 +0300

    ORM configrations updated

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,13 +4,13 @@ import { Call } from './src/db/entities/Call'
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'ai_calls',
   entities: [Call],
-  migrations: ['src/migrations/*.ts'],
+  migrations: ['src/db/migrations/*.ts'],
   synchronize: false,
   logging: false,
 })
