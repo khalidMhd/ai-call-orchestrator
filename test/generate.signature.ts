6d0066ac4cae23a28c061e943f394c6ffb87b00c
khalidmhd <webdev1137@gmail.com>
2025-07-24 16:31:55 +0300

    test HEX updated

--- test/generate.signature.ts
+++ test/generate.signature.ts
@@ -1,5 +1,5 @@
-const crypto = require('crypto');
-const CALLBACK_SECRET = 'supersecret';
+import crypto from 'crypto';
+import { CALLBACK_SECRET } from '../src/utils/constants';
 
 const rawBody = JSON.stringify({
   callId: "5574cf22-ccb1-42fb-94d5-8a04335db6b2",
@@ -8,7 +8,7 @@ const rawBody = JSON.stringify({
   completedAt: "2025-07-23T12:00:00Z"
 });
 
-const signature = crypto.createHmac('sha256', CALLBACK_SECRET)
+const signature = crypto.createHmac('sha256', CALLBACK_SECRET || 'supersecret')
   .update(rawBody)
   .digest('hex');
 
