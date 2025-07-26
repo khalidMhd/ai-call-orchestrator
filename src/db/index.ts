df4e10d1b14426a3ace4fc991bc40b481e25f13f
khalidmhd <webdev1137@gmail.com>
2025-07-26 17:03:01 +0300

    DB logging

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,13 +7,14 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST!,
-  port: parseInt(process.env.DB_PORT!),
-  username: process.env.DB_USERNAME!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Call],
-  synchronize: false,
+  synchronize: true, // for dev only
   // logging: process.env.NODE_ENV !== 'production',
   migrations: [path.resolve(__dirname, 'migrations', '*.{ts,js}')],
+
 });
